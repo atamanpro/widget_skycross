6aa5c1bdf89e1e6ad1d8d55d9535b15434258485
Юлия Седушкина <sed1452002@gmail.com>
2024-12-10 13:48:39 +0200

    comp header rewritten without using style library

--- src/components/BlockHeader/Header.jsx
+++ src/components/BlockHeader/Header.jsx
@@ -1,19 +1,16 @@
-import { Flex, Text, Image, Box } from '@mantine/core';
 import logo from "../../assets/logoSkyCross.svg";
 import arrowLeft from "../../assets/ArrowLeft.svg";
 import "./HeaderStyle.scss";
 
 const Header = () => {
   return (
-    <Flex style={{ alignItems: 'center' }} className='BoxHeader'>
-      <Box className='boxArrowHeader'> 
-        <Image src={arrowLeft} alt="Arrow Left" width={12} height={12} />
-      </Box>
-      <Text className='headerText'>
-        Виктория
-      </Text>
-      <Image src={logo} alt="Logo" width={60} height={43} className='logoHeader' />
-    </Flex>
+    <div className="BoxHeader">
+      <div className="boxArrowHeader">
+        <img src={arrowLeft} alt="Arrow Left" style={{ width: '12px', height: '12px' }} />
+      </div>
+      <p className="headerText">Виктория</p>
+      <img src={logo} alt="Logo" className="logoHeader" />
+    </div>
   );
 };
 
