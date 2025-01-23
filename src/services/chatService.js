c3adc15e5361fd039df1ad4fe050c7630602390f
Юлия Седушкина <sed1452002@gmail.com>
2025-01-23 12:35:16 +0200

    change new link for back

--- src/services/chatService.js
+++ src/services/chatService.js
@@ -12,7 +12,7 @@ export const sendMessageToServer = async (message, images) => {
         };
 
         const response = await axios.post(
-            "https://skycross-in-dev.replit.app/",
+            "https://skycross--widget.replit.app/",
             payload,
             { headers: { "Content-Type": "application/json" } }
         );
